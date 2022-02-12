724e3c58c056835568878c52daaaf5a4fbc85ab6
Bharat Joshi <robotics.bharat@gmail.com>
2022-02-11 19:53:11 -0500

    added opengc 2D-2D matching RANSAC

---
+++ pose_graph/include/utils/LoopClosureUtils.h
@@ -0,0 +1,26 @@
+#include <Eigen/Core>
+#include <opengv/point_cloud/PointCloudAdapter.hpp>
+#include <opengv/relative_pose/CentralRelativeAdapter.hpp>
+#include <opengv/relative_pose/methods.hpp>
+#include <opengv/sac/Ransac.hpp>
+#include <opengv/sac_problems/point_cloud/PointCloudSacProblem.hpp>
+#include <opengv/sac_problems/relative_pose/CentralRelativePoseSacProblem.hpp>
+
+#include "utils/UtilsOpenCV.h"
+using BearingVectors = std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>;
+
+class LoopClosureUtils {
+ private:
+  // Lcd typedefs
+  using AdapterMono = opengv::relative_pose::CentralRelativeAdapter;
+  using SacProblemMono = opengv::sac_problems::relative_pose::CentralRelativePoseSacProblem;
+  using AdapterStereo = opengv::point_cloud::PointCloudAdapter;
+  using SacProblemStereo = opengv::sac_problems::point_cloud::PointCloudSacProblem;
+
+ public:
+  static bool geometricVerificationNister(std::vector<cv::KeyPoint>& matched_2d_old,
+                                          std::vector<cv::Point2f>& matched_2d_old_norm,
+                                          std::vector<uchar>& status,
+                                          int min_correspondences,
+                                          opengv::transformation_t* camMatch_T_camQuery_mono);
+};
